2d1b011c76444952121ce9e23a8b7c272f635f70
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2022-02-14 10:27:19 +0100

    wip

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,12 +3,12 @@ pub fn after_char(s: &str, start: char) -> Option<&str> {
     Some(&s[pos + 1..])
 }
 
-pub fn after<'a>(s : &'a str, start: &'a str) -> Option<&'a str> {
+pub fn after<'a>(s : &'a str, start: &str) -> Option<&'a str> {
     let pos = s.find(start)?;
     Some(&s[pos + start.len() ..])
 }
 
-pub fn after_last<'a>(s : &'a str, start: &'a str) -> Option<&'a str> {
+pub fn after_last<'a>(s : &'a str, start: &str) -> Option<&'a str> {
     let pos = s.rfind(start)?;
     Some(&s[pos + start.len() ..])
 }
@@ -17,16 +17,16 @@ pub fn before<'a>(s: &'a str, end: &'a str) -> Option<&'a str> {
     Some(&s[..s.find(end)?])
 }
 
-pub fn between<'a>(s: &'a str, start: &'a str, end: &'a str) -> Option<&'a str> {
+pub fn between<'a>(s: &'a str, start: &'a str, end: &str) -> Option<&'a str> {
     after(s, start).and_then(|s| before(s, end))
 }
 
-pub fn before_and_after<'a>(s: &'a str, end: &'a str) -> Option<(&'a str, &'a str)> {
+pub fn before_and_after<'a>(s: &'a str, end: &str) -> Option<(&'a str, &'a str)> {
     let pos = s.find(end)?;
     Some((&s[..pos], &s[pos + end.len() ..]))
 }
 
-pub fn between_and_after<'a>(s: &'a str, start: &'a str, end: &'a str) -> Option<(&'a str, &'a str)> {
+pub fn between_and_after<'a>(s: &'a str, start: &'a str, end: &str) -> Option<(&'a str, &'a str)> {
     after(s, start).and_then(|s| before_and_after(s, end))
 }
 
