24cf9280f3e4ac436369b3275e3e136ca344324d
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2022-08-13 12:38:22 +0200

    cleanup: import whole "my_ldap" module (convention)

--- server/cache.ts
+++ server/cache.ts
@@ -1,8 +1,8 @@
+import * as my_ldap from './my_ldap'
 import { internal_error } from "./helpers";
 import ldap_filter from "./ldap_filter";
 import { dn_to_sgroup_id } from "./ldap_helpers";
 import { mono_attrs } from "./ldap_wrapper";
-import { search_sgroups } from "./my_ldap";
 import { hMright, MyMap, Option, RemoteSqlQuery } from "./my_types";
 import { parse_sql_url } from "./remote_query";
 
@@ -19,7 +19,7 @@ export const get_remote_to_sgroup_ids = async () => (
 async function get_remote_to_sgroup_ids_() {
     const attr = hMright.to_attr_synchronized('member');
     const map: RemoteToSgroupIds = {};
-    for (const entry of await search_sgroups(ldap_filter.presence(attr), [attr], undefined)) {
+    for (const entry of await my_ldap.search_sgroups(ldap_filter.presence(attr), [attr], undefined)) {
         const sgroup_id = dn_to_sgroup_id(entry.dn) ?? internal_error();
         const url = mono_attrs(entry)[attr] ?? internal_error()
         const remote: RemoteSqlQuery = parse_sql_url(url) ?? internal_error();
