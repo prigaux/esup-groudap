41a9c762d3a2ba1ca0dc59b98afdd9ab53a440a3
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2022-08-13 12:39:18 +0200

    cleanup: import whole "my_ldap" module (convention)

--- server/my_ldap_check_rights.ts
+++ server/my_ldap_check_rights.ts
@@ -1,7 +1,7 @@
 import ldap_filter from "./ldap_filter"
+import * as my_ldap from './my_ldap'
 import { people_id_to_dn, sgroup_filter } from "./ldap_helpers";
 import { one_group_matches_filter } from "./ldap_wrapper";
-import { is_sgroup_existing, user_has_right_on_sgroup_filter } from "./my_ldap";
 import { Dn, LoggedUser, Right } from "./my_types"
 import { parent_stem, parent_stems } from "./stem_helpers";
 
@@ -9,7 +9,7 @@ export async function user_has_right_on_at_least_one_sgroups(user_dn: Dn, ids: s
     const ids_filter = ldap_filter.or(ids.map(sgroup_filter))
     const filter = ldap_filter.and2(
         ids_filter,
-        user_has_right_on_sgroup_filter(user_dn, right),
+        my_ldap.user_has_right_on_sgroup_filter(user_dn, right),
     );
     
     return await one_group_matches_filter(filter)
@@ -18,7 +18,7 @@ export async function user_has_right_on_at_least_one_sgroups(user_dn: Dn, ids: s
 export async function check_right_on_any_parents(logged_user: LoggedUser, id: string, right: Right) {
     if ('TrustedAdmin' in logged_user) {
         const parent_stem_ = parent_stem(id)
-        if (parent_stem_ && !await is_sgroup_existing(parent_stem_)) { 
+        if (parent_stem_ && !await my_ldap.is_sgroup_existing(parent_stem_)) { 
             throw `stem ${parent_stem_} does not exist`
         }    
     } else {
