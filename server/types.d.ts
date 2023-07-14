791716ffefcf919910d0ea587c2ef69621041c70
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2023-07-14 18:04:44 +0200

    chore: update types for ldapjs-promise-disconnectwhenidle (which is now usable on different LDAP connections)

--- server/types.d.ts
+++ server/types.d.ts
@@ -6,9 +6,12 @@ interface Object {
 
 declare module 'ldapjs-promise-disconnectwhenidle' {
     import * as ldapjs from 'ldapjs'
-    function init(conf: { uri: string[]; dn?: string; password?: string; disconnectWhenIdle_duration?: number; verbose?: boolean }): void
-    function clientP(): ldapjs.Client
+    type conf = { uri: string[]; dn?: string; password?: string; disconnectWhenIdle_duration?: number; verbose?: boolean }
+    function init(conf: conf): void
+    function new_client(conf: conf): ldapjs.Client
+    function may_bind(conf: conf, client: ldapjs.Client): Promise<ldapjs.Client>
     function destroy(): void
+    function searchRaw(base: string, filter: string, attributes: string[], options: ldapjs.SearchOptions, clientP?: Promise<ldapjs.Client>): Promise<ldapjs.SearchEntry[]>
     function search(base: string, filter: string, attributes: string[], options: ldapjs.SearchOptions): Promise<ldapjs.SearchEntryObject[]>
     function add(dn: string, entry: Record<string, unknown>): Promise<void>
     function del(dn: string): Promise<void>
